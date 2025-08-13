62e63a2a19d06c655889aa52022980c878f7fab5
Bohdan Lytvynov <blytv@softserveinc.com>
2025-08-13 16:47:35 +0300

    Add master page and navigation

--- furniture-store/src/app/app.ts
+++ furniture-store/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { Master } from "./master/master";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Master],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
