40c17d3a2d65215eb419bce874c0a3356c6c2c78
Bohdan Lytvynov <blytv@softserveinc.com>
2025-08-17 10:43:03 +0300

    footer finished

--- furniture-store/src/app/master/master.ts
+++ furniture-store/src/app/master/master.ts
@@ -4,10 +4,11 @@ import { DayOffer } from "../day-offer/day-offer";
 import { BestSelling } from "../best-selling/best-selling";
 import { Gallery } from "../gallery/gallery";
 import { Contact } from "../contact/contact";
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-master',
-  imports: [Banner, DayOffer, BestSelling, Gallery, Contact],
+  imports: [Banner, DayOffer, BestSelling, Gallery, Contact, Footer],
   templateUrl: './master.html',
   styleUrl: './master.css'
 })
