aadb63eb9a0844e0cddeac79743d89d2ed85fdec
Bohdan Lytvynov <blytv@softserveinc.com>
2025-08-17 09:44:08 +0300

    gallery finished

--- furniture-store/src/app/master/master.ts
+++ furniture-store/src/app/master/master.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Banner } from "../banner/banner";
 import { DayOffer } from "../day-offer/day-offer";
 import { BestSelling } from "../best-selling/best-selling";
+import { Gallery } from "../gallery/gallery";
 
 @Component({
   selector: 'app-master',
-  imports: [Banner, DayOffer, BestSelling],
+  imports: [Banner, DayOffer, BestSelling, Gallery],
   templateUrl: './master.html',
   styleUrl: './master.css'
 })
